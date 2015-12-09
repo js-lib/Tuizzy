1e9d0781a66b3981189b5b499ce0fa3ed3b33b98
js-lib <tsmithwd+github@gmail.com>
2015-12-09 16:06:19 -0500

    Add getTuizFile() function to fileParse.js

--- js/fileParse.js
+++ js/fileParse.js
@@ -1,1 +1,11 @@
-
+(function(context) {
+  
+  // NOTE: for getTuizFile() to work, the current URL must be in
+  //   the directory containing /tuizzes/
+  // TODO: Check name for validity?
+  var getTuizFile = function(name, callback) {
+    var request = new context.XMLHttpRequest();
+    request.addEventListener('load', callback);
+    request.open('GET', './tuizzes/'+name);
+  };
+})(window);
