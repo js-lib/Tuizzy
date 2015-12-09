62342d8a369d25c79e99e36a97d818d9b1a7b448
js-lib <tsmithwd+github@gmail.com>
2015-12-09 16:10:36 -0500

    Add window.Tuizzy.getTuizObject() to fileParse.js
    Add window.Tuizzy if it doesn't exist; add window.Tuizzy.getTuizObject(), which gets the object form of a .tuiz file by name.

--- js/fileParse.js
+++ js/fileParse.js
@@ -5,7 +5,21 @@
   // TODO: Check name for validity?
   var getTuizFile = function(name, callback) {
     var request = new context.XMLHttpRequest();
-    request.addEventListener('load', callback);
+    request.addEventListener('load', function() {
+      callback.call(this, this.responseText);
+    });
     request.open('GET', './tuizzes/'+name);
+  };
+  
+  if (!window.Tuizzy) {
+    window.Tuizzy = {};
+  }
+  
+  // A simple function to get an object form of a .tuiz file
+  window.Tuizzy.getTuizObject = function(name, callback) {
+    getTuizFile(name, function(content) {
+      var object = JSON.parse(content);
+      callback(object);
+    })
   };
 })(window);
