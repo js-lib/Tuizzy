bbce56bd963e106d740f76fceff72e58212ca87a
js-lib <tsmithwd+github@gmail.com>
2015-12-09 20:50:39 -0500

    Add constructors and getTuiz() to fileParse.js
    Add Question() constructor, Tuiz() constructor, and context.Tuizzy.getTuiz,
    as well as some comments to js/fileParse.js

--- js/fileParse.js
+++ js/fileParse.js
@@ -1,7 +1,7 @@
 (function(context) {
   
   // NOTE: for getTuizFile() to work, the current URL must be in
-  //   the directory containing /tuizzes/
+  //  the directory containing /tuizzes/
   // TODO: Check name for validity?
   var getTuizFile = function(name, callback) {
     var request = new context.XMLHttpRequest();
@@ -12,15 +12,36 @@
     request.send();
   };
   
+  var Question = function(object) {
+    this.timeLimit = object.time; // TODO: convert this to millis
+    // TODO: finish
+  };
+  
+  var Tuiz = function(object) {
+    // TODO: finish
+    // This should contain sections, questions, etc.
+  };
+  
+  
+  // If Tuizzy does not exist (it shouldn't), make it an object
   if (!context.Tuizzy) {
     context.Tuizzy = {};
   }
   
   // A simple function to get an object form of a .tuiz file
+  // NOTE: This function should only be used by HTML if HTML wants
+  //  to directly interact with object - normally ...getTuiz()
+  //  should be used.
   context.Tuizzy.getTuizObject = function(name, callback) {
     getTuizFile(name, function(content) {
       var object = JSON.parse(content);
       callback(object);
     })
+  };
+  
+  context.Tuizzy.getTuiz = function(name, callback) {
+    context.Tuizzy.getTuizObject(name, function() {
+      
+    });
   };
 })(window);
